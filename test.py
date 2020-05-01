5ef11ce2248a4d30f8c065da94998d670c1efe5b
Hila Harhol <hila.harhol@gmail.com>
2020-05-01 18:37:42 +0300

    fix test for creating MT server

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import socket
 
+MSG_LEN = 5
 MSG = "Hello"
 CONFIG_PATH = "..\\Trivia Project\\config.txt"
+MIN_PORT = 1024
+MAX_PORT = 65535
 
 
 def main():
@@ -10,11 +13,15 @@ def main():
     config = open(CONFIG_PATH, 'r')
     lines = list(config)
 
+    # get the ip the and port of the server
     for line in lines:
         if "server_ip=" in line:
             server_ip = line[10:-1]
         elif "port=" in line:
             server_port = int(line[5:])
+            if server_port < MIN_PORT or server_port > MAX_PORT:
+                print("port is invalid!")
+                quit()
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("socket has created")
@@ -28,16 +35,17 @@ def main():
         quit()
     print("socket has connected")
 
-    server_msg = sock.recv(1024)
+    server_msg = sock.recv(MSG_LEN)
     server_msg = server_msg.decode()
-    print(server_msg)
 
-    try:
-        sock.sendall(MSG.encode())
-    except Exception as e:
-        print("ERROR -", e)
-        sock.close()
-        quit()
+    if server_msg == MSG:
+        print(server_msg)
+        try:
+            sock.sendall(server_msg.encode())
+        except Exception as e:
+            print("ERROR -", e)
+            sock.close()
+            quit()
 
     sock.close()
 
